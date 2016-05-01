7eaaa74bc88f458587a873e93279fea523bfdc48
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-04-30 21:40:34 -0700

    Added files via upload
    Initial Code

---
+++ index.php
@@ -0,0 +1,306 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>DJ CJ - A Decade of DJing: Weddings to Nightclubs; Bars to House Parties and Schools Across the Pacific Northwest.</title>
+
+<meta name="description" content="DJ-CJ is a Disc Jockey serving the Pacific Northwest from Sacramento to Portland; specializing in schools, weddings, parties, and the bar and nightclub scene.">
+<meta name="keywords" content="DJ, Disc, Jockey, Chico, Portland, Sacramento, Redding, California, Oregon, Mobile, Party, Music, Wedding, School, Bar, Club, Disc Jockey">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="shortcut icon" href="images/favicon.gif">
+	
+	
+<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
+<META HTTP-EQUIV="EXPIRES" CONTENT="Mon, 22 Jul 2002 11:12:01 GMT">
+<meta name="author" content="CJ Trowbridge">
+<meta name="viewport" content="width=900, initial-scale=1">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@djcj88">
+<meta name="twitter:creator" content="@djcj88">
+<meta name="twitter:title" content="DJ-CJ.com">
+<meta name="twitter:description" content="DJ CJ - A Decade of DJing: Weddings to Nightclubs; Bars to House Parties and Schools Across the Pacific Northwest.">
+<meta name="twitter:image" content="http://dj-cj.com/images/favicon.gif">
+
+<meta property="og:title" content="DJ-CJ.com">
+<meta property="og:type" content="website">
+<meta property="og:url" content="http://dj-cj.com/">
+<meta property="og:image" content="http://dj-cj.com/images/favicon.gif">
+
+<meta name="application-name" content="DJ-CJ.com"/> 
+<meta name="msapplication-TileColor" content="#0071bb"/> 
+<meta name="msapplication-TileImage" content="http://dj-cj.com/images/favicon.gif"/>
+
+<meta charset="UTF-8">
+
+<link rel="apple-touch-icon" href="http://dj-cj.com/images/favicon.gif"/>
+<link rel="apple-touch-startup-image" href="http://dj-cj.com/images/favicon.gif">
+	
+<style>
+	.mix_headline{
+		text-align: center;
+		font-weight: bold;
+		font-size: 20px;
+		padding: 5px;
+	}
+	.mix{
+		padding: 5px;
+		margin: 5px;
+		border: 2px solid #cccccc;
+	}
+	.mix a{
+		color: #000;
+		font-weight: bold;
+	}
+	#contact{
+		text-align:center;
+	}
+	.heading{
+		margin-bottom: 10px;
+		margin-top: -10px;
+		border: 2px solid #cccccc;
+	}
+	.heading img{
+		max-width: 100%;
+	}
+	.clearer{clear:both;}
+	body{
+		min-width: 900px;
+		font-family: Helvetica;
+		background-image:url(images/bg.jpg);
+		background-attachment:fixed;
+	}
+	#wrapper{
+		min-width:900px;
+		width: 900px;
+		margin: 0px auto;
+	}
+	.body_text{
+		padding: 5px;
+	}
+	.body_text h1{
+		margin: 0px;
+		padding: 0px;
+		font-size: 20px;
+	}
+	p{
+		margin: 0px;
+		text-indent: 5px;
+		padding: 5px;
+	}
+	#form{
+		padding: 5px;
+		float: left;
+		width: 210px;
+	}
+	#form .i{
+		width: 200px;
+	}
+	#form textarea{
+		height: 150px;
+	}
+	#footer, #footer a{
+		text-align: center;
+		color: #ffffff;
+		font-size: 12px;
+	}
+	#l_cols{
+		float: left;
+		width: 600px;
+	}
+	#r_cols{
+		float: right;
+		width: 280px;
+		background-color:#FFFFFF;
+		border: 2px solid #cccccc;
+		height: 500px;
+	}
+	#slideshow img{
+		width: 100%;
+		min-width: 100%;
+		max-width: 100%;
+		border: 2px solid #cccccc;
+	}
+	#resume{
+		padding: 5px;
+		font-size: 15px;
+	}
+	h1{
+		margin: 0px; padding: 0px; font-size: 16px; font-weight: bold; text-align:center; margin-bottom: 10px;
+	}
+	h2{
+		margin: 0px; padding: 0px; font-size: 16px;
+	}
+	#resume ul{
+		margin: 0px;
+	}
+	#bottom_box{
+		clear: both;
+		background-color:#FFFFFF;
+		border: 2px solid #cccccc;
+		margin-top: 10px;
+		font-size: 16px;
+	}
+	#gear table{
+		margin: 0px auto;
+	}
+	#gear{
+		padding: 5px;
+	}
+	#bio{
+		background-color:#FFFFFF;
+		border: 2px solid #cccccc;
+		margin-bottom: 10px;
+		font-size: 11px;
+		text-align:center;
+	}
+	#bio_bottom{
+		padding: 5px;
+	}
+	#form h2{
+		margin-bottom: 5px;
+	}
+</style>
+	
+	
+	
+<script type="text/javascript">
+var image1=new Image();
+image1.src="images/dj-cj-018.jpg";
+var image2=new Image();
+image2.src="images/dj-cj-001.jpg";
+var image3=new Image();
+image3.src="images/dj-cj-012.jpg";
+var image4=new Image();
+image4.src="images/dj-cj-013.jpg";
+var image5=new Image();
+image5.src="images/dj-cj-002.jpg";
+var image6=new Image();
+image6.src="images/dj-cj-003.jpg";
+var image7=new Image();
+image7.src="images/dj-cj-004.jpg";
+var image8=new Image();
+image8.src="images/dj-cj-011.jpg";
+var image9=new Image();
+image9.src="images/dj-cj-005.jpg";
+var image10=new Image();
+image10.src="images/dj-cj-006.jpg";
+var image11=new Image();
+image11.src="images/dj-cj-007.jpg";
+var image12=new Image();
+image12.src="images/dj-cj-008.jpg";
+var image13=new Image();
+image13.src="images/dj-cj-009.jpg";
+var image14=new Image();
+image14.src="images/dj-cj-010.jpg";
+</script>
+</head>
+<body>
+<div id="wrapper">
+	<div id="content">
+		<div class="heading">
+			<img src="images/banner4.gif"  width="896" height="100" alt="DJ CJ - A Decade of DJing: Weddings to Nightclubs; Bars to House Parties and Schools Across the Pacific Northwest.">
+		</div>
+		<div id="bio">
+			<p>I'm a Mobile DJ serving the Pacific Northwest from Sacramento to Portland. From 
+			clubs to bars to private parties and schools, I've 
+			done it all since '03.</p>
+		</div>
+		<div class="clearer"></div>
+		<div id="l_cols">
+			<div id="slideshow">
+				<img name="slideshow" width="600" height="500" id="slideshow" src="images/dj-cj-018.jpg" alt="DJ CJ" />
+			</div>
+		</div>
+		<div id="r_cols">
+			<div id="resume">
+				<h2>Bars and Nightclubs</h2>
+				<ul>
+					<li>Panama Bar and Cafe</li>
+					<li>The Maltese Bar and Taproom</li>
+					<li>The 501 Club</li>
+					<li>The University Bar</li>
+				</ul><br>
+				<h2>Schools</h2>
+				<ul>
+					<li>Shasta Lake School</li>
+					<li>Enterprise High School</li>
+					<li>Central Valley High School</li>
+					<li>Buckeye Elementary</li>
+					<li>Parsons Elementary</li>
+					<li>Cow Creek School</li>
+					<li>Milville Elementary</li>
+				</ul><br>
+				<h2>Other Venues</h2>
+				<ul>
+					<li>Forever 21</li>
+					<li>Shasta Dam Concourse</li>
+					<li>Churn Creek Golf Course</li>
+					<li>The Arroyo Room</li>
+					<li>Alpha Chi Sorority</li>
+					<li>Subud Hall</li>
+					<li>Tale O' the Whale Restaurant</li>
+					<li>American Legion Hall</li>
+					<li>Lots of Weddings and Parties</li>
+				</ul><br>
+				<h2>And Lots More</h2>
+				<div class="clearer"></div>
+			</div>
+			<div class="clearer"></div>
+		</div>
+		<div class="clearer"></div>
+		<div id="bottom_box">
+			<div class="mix_headline">Download My Free Sample Mixes!</div>
+			<div class="mix">
+				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/122797726&amp;color=ff6600&amp;auto_play=true&amp;show_artwork=true"></iframe>
+				<br><br><h2>Progressive Dance (Click Here to Download Now!)</h2>
+				<p>This mix contains 24 progressive dance tracks, from a great mix session 
+				which demonstrates my favorite style of music; fast paced, high energy dance.
+				I take you from hip-hop to dubstep and everywhere in between. This is a CD
+				you're sure to leave in the car for a long time to come! Be aware, this 
+				mix does contain explicit lyrics.</p>
+			</div>
+		</div>
+		<div class="clearer"></div>
+		<div id="bottom_box">
+			<div id="gear">
+				<table>
+					<tr>
+						<td><img src="images/eon-r.gif" height="100" alt="JBL EON"></td>
+						<td><img src="images/ssl.gif" height="100" alt="Serato Scratch LIVE"></td>
+						<td><img src="images/mixer.png" height="100" alt="Denon X500 and HC4500"></td>
+						<td><img src="images/eon-l.gif" height="100" alt="JBL EON"></td>
+					</tr>
+				</table><br>
+				<p>I bring a top-of-the-line sound system, a lighting rig and a huge collection of the most 
+				up to date content available. My mixer and controller are a Denon X500 and a Denon HC4500 and 
+				sound is provided by JBL Eon powered loudspeakers. Only the best.</p>
+				<br>
+				<div id="contact">
+					<b>Phone:</b> 530.953.9499
+					<b>Email:</b> <a href="mailto:chris.j.trowbridge@gmail.com">chris.j.trowbridge@gmail.com</a>
+				</div>
+			</div>
+			<div class="clearer"></div>
+		</div>		
+	</div>
+</div>
+<div class="clearer"> &nbsp;</div>
+<div id="footer">
+	Copyright <a href="http://www.cjtrowbridge.com/" target="_blank">Chris Trowbridge</a> 2010, All Rights Reserved.<br>
+</div>
+<script type="text/javascript">
+var step=1;
+function slideit(){
+if (!document.images)
+return
+document.images.slideshow.src=eval("image"+step+".src");
+if (step<14)
+step++;
+else
+step=1;
+setTimeout("slideit()",5000);
+}
+slideit();
+</script>
+</body>
+</html>
