255a3f6530a7f4aee238ae823d714b4bd711b32a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-05-01 12:21:19 -0700

    Update index.php

--- index.php
+++ index.php
@@ -40,14 +40,30 @@
 							facebook like button
 						</div-->
 						<nav class="navbar navbar-default">
-  							<div class="container-fluid">
-								<ul class="nav navbar-nav">
-									<li><a href="#">Foo</a></li>
-								</ul>
-								<ul class="nav navbar-nav navbar-right">
-									<li><a href="#">Bar</a></li>
-								</ul>
-							</div>
+							<div class="container-fluid">
+								<div class="navbar-header">
+									<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-5" aria-expanded="false">
+										<span class="sr-only">
+											Toggle navigation
+										</span>
+										<span class="icon-bar">
+											
+										</span>
+										<span class="icon-bar">
+											
+										</span>
+										<span class="icon-bar">
+											
+										</span>
+									</button>
+									<a class="navbar-brand" href="#">Brand</a> 
+								</div>
+								<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-5">
+									<p class="navbar-text navbar-right">
+										Signed in as <a href="#" class="navbar-link">Mark Otto</a>
+									</p>
+								</div> 
+							</div> 
 						</nav>
 					</div>
 				</div>
